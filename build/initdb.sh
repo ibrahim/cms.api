941ef3786e303d420f4579a973dd0db8dc5bdbdf
Ibrahim Ahmed <ia.ibrahim@gmail.com>
2018-02-15 07:39:15 +0000

    update docker files for compute engine

--- build/initdb.sh
+++ build/initdb.sh
@@ -1,3 +1,4 @@
 
-docker exec -it mysql mysql -p -e 'create database cms;'
-cat db/structure.sql | docker exec -i mysql mysql -p123456 -u root -D cms
+docker exec -it mysql mysql -p -e 'create database cms;create database cms_test;'
+docker exec -i cmsapi rails db:schema:load
+#cat db/structure.sql | docker exec -i mysql mysql -p123456 -u root -D cms
